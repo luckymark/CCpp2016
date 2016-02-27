274ce3a61a1e24fcafcf484d0aeed0c3c38dadf9
thekoc <robot.koc@outlook.com>
2016-02-27 13:23:22 +0800

    method 1 completed

---
+++ p05_allPrimes/all_primes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include "prime.h"
+
+// int filter(int a[], int length, (int *(int)) cond) {
+//     for (size_t i = 0; i < length; i++) {
+//         /* code */
+//     }
+// }
+
+int main(int argc, char const *argv[]) {
+    // Miller Rabin test
+    clock_t start = 0, end = 0, during = 0;
+    start = clock();
+    for (size_t i = 2; i < 1000000; i++) {
+        if (is_prime(i)) {
+            // printf("%lu\n", i);
+        }
+    }
+    end = clock();
+    during = end - start;
+    printf("method 1 cost %lf sec\n", (double) during / CLOCKS_PER_SEC);
+
+    // sieve of Eratosthenes
+    start = clock();
+
+    return 0;
+}
---
+++ p05_allPrimes/prime.c
@@ -0,0 +1,61 @@
+#include "prime.h"
+#include <stdlib.h>
+
+#define MAGIC_NUM 100
+
+
+unsigned long random_in(unsigned long range);
+
+unsigned long square(unsigned long a) {
+    return a * a;
+}
+
+unsigned long expmod(unsigned long base, unsigned long expo, unsigned long m) {
+    if (expo == 0) {
+        return 1;
+    } else if (expo % 2 == 0) {
+        unsigned long mod = expmod(base, expo / 2, m);
+        unsigned long sqmod = square(mod);
+        if (mod != 1 && mod != m - 1 && sqmod % m == 1) {
+            return 0;
+        } else {
+            return sqmod % m;
+        }
+    } else {
+        return expmod(base, expo - 1, m) * base % m;
+    }
+}
+
+int is_prime(unsigned long p) {
+    if (p % 2 == 0) {
+        return 0;
+    } else {
+        unsigned long a;
+        a = random_in(p);
+        for (size_t i = 0; i < MAGIC_NUM; i++, a = random_in(p)) {
+            int result = expmod(a, p - 1, p);
+            if (result == 0 || result != 1) {
+                return 0;
+            }
+        }
+        return 1;
+    }
+}
+
+// Thank to god that we have such a "convient" random funtion in C so that
+// I have to rewrite all of these useless function to genertae just one tiny
+// number
+
+unsigned long random_in_dec(unsigned long range) {
+    if (range == 0) {
+        return rand() % 10;
+    } else {
+        return rand() % 10 + 10 * random_in_dec(range / 10);
+    }
+}
+
+unsigned long random_in(unsigned long range) {
+    unsigned long random_num;
+    while ((random_num = random_in_dec(range)) >= range || !random_num);
+    return random_num;
+}
