2199892225fdf79b468fd7f645de621fd5c4805a
Pan Haiqi <978005581@qq.com>
2016-03-03 00:02:25 +0800

    isprime

---
+++ practices/level1/p02_isPrime/isprime.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,i;
+	char flag;
+	scanf("%d",&n);
+	flag=1;
+	if(2==n)
+	{
+		printf("yes");
+	}
+	else
+	{
+		for(i=2;i<n;i++)
+		{
+			if(0==n%i)
+			{
+				flag=0;
+			}
+		}
+		if(1==flag)
+		{
+			printf("yes");
+		}
+		else
+		{
+			printf("no");
+		}
+		
+	}
+	return 0;
+}
