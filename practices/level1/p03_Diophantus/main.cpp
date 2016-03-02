c47d40130cd644e63673c7b7444f3fd99d5e9eb9
iam24 <625083280@qq.com>
2016-03-02 19:31:24 +0800

    add Diophantus

---
+++ practices/level1/p03_Diophantus/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Diophantus
+//
+//  Created by iam24 on 16/2/29.
+//  Copyright © 2016年 iam24. All rights reserved.
+//
+#include <iostream>
+bool can(int n)
+{
+    if (n & 1) return false;
+    if (n % 6 || n % 12 || n % 7) return false;
+    int son = n / 2;
+    int tot = n/12 + n/7 + n/6;
+    if (tot == (n-son-5-4)) return true;
+    return false;
+}
+using namespace std;
+int main()
+{
+    for ( int i = 10 ;i <= 200 ;i++)
+    {
+        if (can(i))
+        {
+            cout<<"he was "<<i<<" years old when he died."<<endl;
+            return 0;
+        }
+    }
+    return 0;
+}
