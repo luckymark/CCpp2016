d1f537ca0ba28c7ec8be1834f417bb5f4897ddfa
Pan Haiqi <978005581@qq.com>
2016-03-03 00:05:38 +0800

    p03_diophantus

---
+++ practices/level1/p03_Diophantus/p03_Diophantus.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	float x=0;
+	while(1)
+	{
+		if(x/6+x/12+x/7+5+4+x/2==x)
+		{
+			printf("%.f",x-4);
+			break;
+		}
+		else
+		{
+			x++;
+		}
+	    
+	}
+	return 0;
+	
+	
+}
