8299306fd07b1f175dd7a1844b8b8f4dc01e7487
HELLHELLO <498523614@qq.com>
2016-03-03 19:02:31 +0800

    to 07

--- practices/level1/p05_allPrimes/allPrimes.c
+++ practices/level1/p05_allPrimes/allPrimes.c
@@ -1,28 +1,44 @@
-#include <stdio.h>
+221#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
 
+void findPrimes(int* primes,int max);
+void printfPrime(int* primes,int max);
+
 int main()
 {
-    int i,n,j,k,m=1;                            //k用来标记是否被整除，m用来表示素数数组中最后一项的下标
-    int a[500]={2,3};                           //a为素数数组
-    printf("2\t3\t");
-    for(i=5;i<1000;i+=2){
+    int primes[1000]={0};
+    int max=1000;
+    findPrimes(primes,max);
+    printfPrime(primes,max);
+    printf("\nThe time taken:%fs",(double)clock()/CLOCKS_PER_SEC);
+    return 0;
+}
+
+void findPrimes(int* primes,int max){
+    int i,n,j,k,m=2;
+    primes[0]=2;
+    primes[1]=3;
+    for(i=5;i<max;i+=2){
         k=0;                                    //新循环开始时置标记数为0，表示还未发现可被整除
         n=(int)sqrt(i);
-        for(j=0;a[j]!=0&&a[j]<=n;j++){
-            if(i%a[j]==0){
+        for(j=0;primes[j]!=0&&primes[j]<=n;j++){
+            if(i%primes[j]==0){
                 k=1;                            //发现素因子，置标记为1并跳出循环
                 break;
             }
         }
-        if(k==0){                                //标记为0表示未发现素因子，为素数，打印该数并将其存入数组
+        if(k==0){                                //标记为0表示未发现素因子，为素数，将其存入数组
+            primes[m]=i;
             m+=1;
-            a[m]=i;
-            printf("%d\t",i);
         }
     }
-    printf("\nThe time taken:%fs",(double)clock()/CLOCKS_PER_SEC);
-    return 0;
+}
+
+void printfPrime(int* primes,int max){
+    int i;
+    for(i=0;primes[i]!=0;i++){
+        printf("%d\t",primes[i]);
+    }
 }
