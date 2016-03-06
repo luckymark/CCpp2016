a485451a9469cc7b42359441ca6103cc442fb212
xian <pzhxbz@QQ.com>
2016-03-06 16:11:14 +0800

    Update Diophantus.c

--- practices/level1/p03_Diophantus/Diophantus.c
+++ practices/level1/p03_Diophantus/Diophantus.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    float y1,y2;
     for(int i=2;i<190;i++)
     {
+        float y1,y2;
         y1=i;
         y2=y1/2;
         if(y1*17/28-9-y2<1e-6&&y1*17/28-9-y2>-1e-6)
