695e88a01f76bfd2b881cc02685e88c1bac527c1
wxdao <waxiadao@gmail.com>
2016-03-06 13:39:47 +0800

    new method

--- practices/level1/p05_allPrimes/main.cpp
+++ practices/level1/p05_allPrimes/main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <cmath>
+#include <cstring>
 #include <sys/time.h>
 
 using namespace std;
 
 long getCurrentTime();
 
+#define MAX 1000
+
 int main(int argc, char *argv[]) {
+    int isPrime[MAX + 1];
+    memset(isPrime, 1, MAX + 1);
+    isPrime[1] = 0;
     auto startT = getCurrentTime();
-    for (unsigned i = 2; i <= 1000; ++i) {
-        if (isPrime(i)) {
-            cout << i << endl;
+    for (unsigned i = 2; i <= sqrt(MAX); ++i) {
+        if (isPrime[i]) {
+            for (unsigned j = 2; j <= MAX / i; ++j) {
+                isPrime[i * j] = 0;
+            }
         }
     }
     auto endT = getCurrentTime();
+    for (unsigned i = 1; i < MAX + 1; ++i) {
+        if (isPrime[i]) {
+            cout << i << endl;
+        }
+    }
     cout << "time(us): " << endT - startT;
     return 0;
 }
