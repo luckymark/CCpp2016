f78237240dafdf82923c2f817143625e12c019b5
xian <pzhxbz@QQ.com>
2016-03-06 16:11:42 +0800

    Update p04_narcissus.c

--- practices/level1/p04_ narcissus/p04_narcissus.c
+++ practices/level1/p04_ narcissus/p04_narcissus.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int n=10,i,j,k,l,m;
-    for(n=100;n<1000;n++)
+    for(int n=100;n<1000;n++)
+    {
+        int i,j,k,l,m;
+        m=n%100;//去掉百位上的数
+        k=(n-m)/100;//求出百位上的数
+        l=m%10;//求出个位的数
+        m=(m-l)/10;//求出十位的数
+        if(n==m*m*m+l*l*l+k*k*k)//m为十位的数，k为百位的数，l为个位的数
         {
-            m=n%100;
-            k=(n-m)/100;
-            l=m%10;
-            m=(m-l)/10;
-            if(n==m*m*m+l*l*l+k*k*k)
-           {
-               printf("%d\n",n);
-           }
+            printf("%d\n",n);
         }
-     return 0;
+    }
+    return 0;
 }
