17fd1934c02e1ef0c28e8a98061a5408dfb43c4f
Fragile-azalea <1872891695@qq.com>
2016-03-06 13:19:22 +0800

    Update Goldbach.cpp
    太生疏了 这样子改对吗

--- practices/level1/p06_Goldbach/Goldbach.cpp
+++ practices/level1/p06_Goldbach/Goldbach.cpp
@@ -1,34 +1,39 @@
 #include<cstdio>
-#include<cstring>
-const int MAX = 100;
-void selectAllPrime(int n);
-bool goldbach(int n);
-bool prime[MAX+10];
+#define MAX 100
+void selectAllPrime(bool primes[],int n);
+bool goldbach(bool primes[],int n);
 int main() {
-    selectAllPrime(MAX);
-    printf("Goldbach is %s",goldbach(MAX)?"Truth":"False");
+    bool prime[MAX + 10];
+    selectAllPrime(prime, MAX);
+    printf("Goldbach is %s", goldbach(prime, MAX) ? "Truth" : "False");
     return 0;
 }
-void selectAllPrime(int n) {
-    memset(prime, 1, sizeof(prime));
-    prime[1] = 0;
+void selectAllPrime(bool primes[],int n) {
+    for (int i = 1; i <= MAX; ++i) {
+        primes[i] = 1;//init;
+    }
     for (int i = 2; i <= MAX; ++i) {
-        if (!prime[i])continue;//because of it had been out
+        if (!primes[i])continue;//because of it had been out
         for (int j = i + i; j <= MAX; j += i) {
-            prime[j] = 0;//because of i is a factor of j
+            primes[j] = 0;//because of i is a factor of j
         }
     }
+    /*for (int i = 1; i <= MAX; ++i) {
+        if (primes[i]) {
+            printf("%d ", i);//check
+        }
+    }*/
 }
-bool goldbach(int n){
+bool goldbach(bool primes[],int n) {
     int j;
-    for(int i=4;i<=n;i+=2){
-        for(j=2;j<i;++j){
-            int k=i-j;
-            if(prime[k]&&prime[j]){
+    for (int i = 4; i <= n; i += 2) {
+        for (j = 2; j < i; ++j) {
+            int k = i - j;
+            if (primes[k] && primes[j]) {
                 break;//find it
             }
         }
-        if(j==i){//means no such a pair of j,k satisfied j+k==i && both j and k are primes
+        if (j == i) {//means no such a pair of j,k satisfied j+k==i && both j and k are primes
             return 0;
         }
     }
