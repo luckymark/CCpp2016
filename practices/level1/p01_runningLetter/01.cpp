d1254f24634835c7df06f6321cad53c477e9cab9
phq1234 <978005581@qq.com>
2016-03-07 23:12:14 +0800

    01 again

--- practices/level1/p01_runningLetter/01.cpp
+++ practices/level1/p01_runningLetter/01.cpp
@@ -1,26 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define num 78 
+#include<math.h>
+#define CONSOLE_WIDTH 78 
 int main()
 {
-	int i,j;
-	for(i=0;i<=num;i++)
+	
+	for(int i=0;i<=2*CONSOLE_WIDTH;i++)
 	{
-		for(j=0;j<=i;j++)
+		for(int j=0;j<=(CONSOLE_WIDTH-abs(CONSOLE_WIDTH-i));j++)
 		{
 			printf(" ");
 		}
 		printf("A");
 		system("cls");
 	}
-	for(i=0;i<=num;i++)
-	{
-		for(j=0;j<=num-i;j++)
-		{
-			printf(" ");
-		}
-		printf("A");
-		system("cls");
-	}
+	
 	return 0;
 }
