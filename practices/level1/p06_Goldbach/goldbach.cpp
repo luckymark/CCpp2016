766fe4adf992064e4988f7eb1a569be9f890a15b
Tan Song <tansongsong@gmail.com>
2016-03-07 19:31:44 +0800

    modify goldbach.cpp

--- practices/level1/p06_Goldbach/goldbach.cpp
+++ practices/level1/p06_Goldbach/goldbach.cpp
@@ -1,20 +1,24 @@
 #include <cstdio>
 #include <cstring>
+
 #define PRIME_SIZE 100
-bool prime_vis[PRIME_SIZE];
+
+bool prime_vis[PRIME_SIZE + 10];
 int prime[PRIME_SIZE],prime_cnt;
+
 void getPrime(){
-    memset(prime_vis,0,sizeof(prime_vis));
+    memset(prime_vis, 0, sizeof(prime_vis));
     prime_cnt = 0;
-    for(int i = 2; i <= 100; ++i){
+    for(int i = 2; i <= PRIME_SIZE; ++i){
         if(!prime_vis[i]){
             prime[prime_cnt++] = i;
-            for(int j = i; j <= 100; j += i){
+            for(int j = i; j <= PRIME_SIZE; j += i){
                 prime_vis[j] = true;
             }
         }
     }
 }
+
 bool check(int prime[], bool prime_vis[], int prime_cnt){
     for(int num = 4; num <= 100; num += 2){
         bool flag = false;
@@ -24,13 +28,16 @@ bool check(int prime[], bool prime_vis[], int prime_cnt){
                 break;
             }
         }
-        if(!flag)return false;
+        if(!flag){
+            return false;
+        }
     }
     return true;
 }
+
 int main(){
     getPrime();
-    bool result = check(prime,prime_vis,prime_cnt);
+    bool result = check(prime, prime_vis, prime_cnt);
     if(result){
         printf("YES\n");
     }else{
