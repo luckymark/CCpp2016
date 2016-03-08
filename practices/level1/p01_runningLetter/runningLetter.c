b3c26b3b19f1ccee9257d2d18b04e8f8e9a15765
xian <pzhxbz@QQ.com>
2016-03-08 20:14:53 +0800

    Update runningLetter.c

--- practices/level1/p01_runningLetter/runningLetter.c
+++ practices/level1/p01_runningLetter/runningLetter.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-#include<math.h>
 #include<stdlib.h>
+#define WIDTH 78
+#define abs(a) (a>0?a:-a)
 int main()
 {
-    for(int i=78;i>=-78;i--)
+    for(int i=WIDTH;i>=-WIDTH;i--)
     {
         int j;
         j=abs(i);
-        for(int k=78;k>j;k--)
+        for(int k=WIDTH;k>j;k--)
         {
             printf(" ");
         }
