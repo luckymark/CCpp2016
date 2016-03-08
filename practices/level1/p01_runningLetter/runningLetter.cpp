394967f3bba9a0c88fedd54986ea3f39e399906c
fkh12345 <1739321550@qq.com>
2016-03-08 16:47:48 +0800

    Added files via upload

---
+++ practices/level1/p01_runningLetter/runningLetter.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 50
+int main()
+{
+	char s[max+1];
+	for (int i = 1; i < max; i++)
+		s[i] = ' ';
+	s[max] = '\0';
+	s[0] = 'a';
+	printf("%s", s);
+	for (int i = 0; i < max - 1; i++)
+	{
+		s[i + 1] = s[i];
+		s[i] = ' ';
+		printf("%s", s);
+		system("cls");
+	}
+	for (int i = max - 1; i > 1; i--)
+	{
+		s[i - 1] = s[i];
+		s[i] = ' ';
+		printf("%s", s);
+		system("cls");
+	}
+	system("pause");
+}
