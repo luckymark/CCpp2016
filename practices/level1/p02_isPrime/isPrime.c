09de776cf63edca8883384a038f0011406595df7
xian <pzhxbz@QQ.com>
2016-03-08 19:46:23 +0800

    Update isPrime.c

--- practices/level1/p02_isPrime/isPrime.c
+++ practices/level1/p02_isPrime/isPrime.c
@@ -1,28 +1,16 @@
 #include<stdio.h>
-char *prime_judge(int a)
-{
-    int flag=0;
-    for(int i=2;i<a;i++)
-    {
-        if(a%i==0)
-        {
-           flag+=1;
-           break;
-        }
-    }
-    if(flag==0)
-    {
-        return "Yes";
-    }
-    else
-    {
-        return "No";
-    }
-}
+#include"prime.h"
 int main()
 {
     int a;
     scanf("%d",&a);
-    printf("%s",prime_judge(a));
+    if(is_prime(a))
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
     return 0;
 }
