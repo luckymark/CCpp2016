4ad67aee6836f03e080a896edd828c12611c4d0b
Ferris <fynet86@gmail.com>
2016-03-10 09:37:58 +0800

    finish p08

---
+++ practices/level1/p08_hanoi/main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  hanoi
+//
+//  Created by Ferris on 16/3/10.
+//  Copyright © 2016年 Ferris. All rights reserved.
+//
+
+#include <stdio.h>
+
+void hanoi(char A,char B,char C,int n)
+{
+    if(n==1)
+    {
+        printf("把第 %d 块从 %c 移到 %c\n",n,A,C);
+    }
+    else
+    {
+        hanoi(A,C,B,n-1);
+        printf("把第 %d 块从 %c 移到 %c\n",n,A,C);
+        hanoi(B,A,C,n-1);
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    int n;
+    printf("输入阶数：\n");
+    scanf("%d",&n);
+    hanoi('A','B','C',n);
+    return 0;
+}
+
+
+    
+
+
+
