1d382d1f24b71018d42757c6f54419443b10bb03
Fragile-azalea <1872891695@qq.com>
2016-03-10 09:08:09 +0800

    Update hanoi.cpp

--- practices/level1/p08_hanoi/hanoi.cpp
+++ practices/level1/p08_hanoi/hanoi.cpp
@@ -1,15 +1,20 @@
 #include<cstdio>
 #define MAX 64
-void hanoi(int n,char from='A',char to='C',char via='B') {
-    if (n == 1) {
-        printf("%c --> %c\n", from, to);
-    } else {
-        hanoi(n - 1, from, via, to);
-        printf("%c --> %c\n", from, to);
-        hanoi(n - 1, via, to, from);
-    }
-}
+void hanoi(int n,char from='A',char to='C',char via='B');
+void print(char from,char to);
 int main() {
     hanoi(MAX);
     return 0;
+}
+void print(char from,char to) {
+    printf("%c --> %c\n", from, to);
+}
+void hanoi(int n,char from,char to,char via) {
+    if (n == 1) {
+        print(from, to);
+    } else {
+        hanoi(n - 1, from, via, to);
+        print(from, to);
+        hanoi(n - 1, via, to, from);
+    }
 }
