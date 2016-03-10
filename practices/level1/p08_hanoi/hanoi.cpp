ff44467630103288efc1fd84759a63c3310894bc
iam24 <625083280@qq.com>
2016-03-10 09:43:17 +0800

    rewrite hanoi

--- practices/level1/p08_hanoi/hanoi.cpp
+++ practices/level1/p08_hanoi/hanoi.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
+void hano_tower(char a, char b, char c, int p);
+int main()
+{
+    int n ;
+    cout<<"请输入盘子数量："<<endl;
+    cin>>n;
+    hano_tower('A', 'B', 'C', n);
+    return 0;
+}
 void hano_tower(char a, char b, char c, int p)
 {
     if (p == 1)
@@ -11,12 +20,4 @@ void hano_tower(char a, char b, char c, int p)
     hano_tower(a, c, b, p-1);
     printf("%c->%c\n",a,c);
     hano_tower(b, a, c, p-1);
-}
-int main()
-{
-    int n ;
-    cout<<"请输入盘子数量："<<endl;
-    cin>>n;
-    hano_tower('A', 'B', 'C', n);
-    return 0;
 }
