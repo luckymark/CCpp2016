736e5f65ccaead5ded7303e31e5d2a242640caf3
HELLHELLO <498523614@qq.com>
2016-03-10 08:54:48 +0800

    p08 再次优化

--- practices/level1/p08_hanoi/hanoi.c
+++ practices/level1/p08_hanoi/hanoi.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int move(int n,char from,char to);
 int hanoi(int n,char from,char to,char other);
 
 int main()
@@ -11,17 +10,13 @@ int main()
     return 0;
 }
 
-int move(int n,char from,char to){
-    printf("%d %c->%c\n",n,from,to);
-    return 0;
-}
 
 int hanoi(int n,char from,char to,char other){
     if(n==1){
-        move(n,from,to);
+        printf("%d %c->%c\n",n,from,to);
     }else{
         hanoi(n-1,from,other,to);
-        move(n,from,to);
+        printf("%d %c->%c\n",n,from,to);
         hanoi(n-1,other,to,from);
     }
     return 0;
