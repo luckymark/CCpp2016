bd5bcdf7d24cde5eae32bbe19714f39e8df68033
fkh12345 <1739321550@qq.com>
2016-03-12 19:37:40 +0800

    Update Diophantus.cpp

--- practices/level1/p03_Diophantus/Diophantus.cpp
+++ practices/level1/p03_Diophantus/Diophantus.cpp
@@ -5,7 +5,9 @@ int main()
 	for (double i = 0; i < 200.0; i++)
 	{
 		if (i / 6.0 + i / 12.0 + i / 7.0 + i / 2.0 + 5 + 4 == i)
-			printf("%.0lf\n", i);
+			{
+				printf("%.0lf\n", i);
+			}
 	}
 	system("pause");
 }
