595244682004aa8f7064c159383294a427273a2f
fkh12345 <1739321550@qq.com>
2016-03-12 19:39:21 +0800

    Update narcissus.cpp

--- practices/level1/p04_ narcissus/narcissus.cpp
+++ practices/level1/p04_ narcissus/narcissus.cpp
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
-int func(int a);
-void print(int,int);
+int narcissus(int a);
+void print(int s,int d);
 int main()
 {
 	int n, a;
 	for (n = 100; n < 1000; n++)
 	{
-		a = func(n);
+		a = narcissus(n);
 		print(a, n);
 	}
 	system("pause");
 }
-int func(int a)
+int narcissus(int a)
 {
 	int b, c, d;
 	b = a / 100;
@@ -25,5 +25,7 @@ int func(int a)
 void print(int s,int d)
 {
 	if (s)
+	{
 		printf("%d", d);
+	}
 }
