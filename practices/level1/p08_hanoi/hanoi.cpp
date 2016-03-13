1e17e38bb51def635caceed21d3430518a362514
fkh12345 <1739321550@qq.com>
2016-03-13 14:54:21 +0800

    Added files via upload

---
+++ practices/level1/p08_hanoi/hanoi.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 4
+int hanoi(char from, char by, char to, int n);
+void print(char from, char to);
+int main()
+{
+	char from = 'A', by = 'B', to = 'C';
+	hanoi(from, by, to, max);
+	system("pause");
+}
+int hanoi(char from, char by, char to, int n)
+{
+	if (n == 0)
+		return 0;
+	hanoi(from, to, by, n - 1);
+	print(from, to);
+	hanoi(by, from, to, n - 1);
+}
+void print(char b, char c)
+{
+	printf("%c->%c\n", b, c);
+}
