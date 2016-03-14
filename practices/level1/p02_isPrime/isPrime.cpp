2f0d47bf4403128885808b506cfbfaf8fef30f80
fkh12345 <1739321550@qq.com>
2016-03-14 21:23:42 +0800

    Update isPrime.cpp

--- practices/level1/p02_isPrime/isPrime.cpp
+++ practices/level1/p02_isPrime/isPrime.cpp
@@ -1,33 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-int Prime(int n);
+int isPrime(int n);
 void print(int r);
 int main()
 {
 	int n, result;
 	while (scanf("%d", &n)!=0)
 	{
-		result = Prime(n);
+		result = isPrime(n);
 		print(result);
 		
 	}
 	system("pause");
 }
-int Prime(int n)
+int isPrime(int n)
 {
 	for (int i = 2; i < n; i++)
+	{
+		if (n%i == 0)
 		{
-			if (n%i == 0)
-				return 0;
-			
+			return 0;
 		}
+	}
 	return 1;
 }
 void print(int r)
 {
 	if (r)
+	{	
 		printf("yes!\n");
+	}
 	else
+	{
 		printf("no!\n");
+	}
 }
