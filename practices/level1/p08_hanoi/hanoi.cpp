bf47f04df7761248b22e64764cdad1e73701ca84
fkh12345 <1739321550@qq.com>
2016-03-14 21:34:28 +0800

    Update hanoi.cpp

--- practices/level1/p08_hanoi/hanoi.cpp
+++ practices/level1/p08_hanoi/hanoi.cpp
@@ -1,18 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define max 4
 int hanoi(char from, char by, char to, int n);
 void print(char from, char to);
 int main()
 {
 	char from = 'A', by = 'B', to = 'C';
-	hanoi(from, by, to, max);
+	hanoi(from, by, to, 64);
 	system("pause");
 }
 int hanoi(char from, char by, char to, int n)
 {
 	if (n == 0)
+	{
 		return 0;
+	}
 	hanoi(from, to, by, n - 1);
 	print(from, to);
 	hanoi(by, from, to, n - 1);
