a6434bf235943ad4e1463f6174ce8e9915aa74ba
mufengjun260 <mufengjun260@163.com>
2016-03-14 15:24:51 +0800

    wrong

---
+++ practices/level1/p08_hanoi/hanoi.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+long num=0;
+
+
+void move(char fm, char to) {
+	num++;
+	cout << "move from " << fm << " to " << to << endl;
+}
+
+void hanoi(int n, char from, char base, char to) {
+	if (n == 1) {
+		move(from, to);
+		return;
+	}
+	else {
+		hanoi(n - 1, from, to, base);	
+		move(from, to);
+		hanoi(n - 1, base, from, to);
+	}
+
+}
+int main() {
+	int n;
+	DWORD StartTime = GetTickCount64();
+
+	cin >> n;
+	hanoi(n, 'A', 'B', 'C');
+	cout <<"CalCount: "<< num<<endl;
+	DWORD EndTime = GetTickCount64();
+	cout << "time: " << EndTime - StartTime<<" ms";
+	system("pause");
+	return 0;
+}
