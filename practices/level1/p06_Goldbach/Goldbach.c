9bdba51ad0efaa88467f5f3da7fd1e849c1457b5
xian <pzhxbz@QQ.com>
2016-03-21 15:21:57 +0800

    Update Goldbach.c

--- practices/level1/p06_Goldbach/Goldbach.c
+++ practices/level1/p06_Goldbach/Goldbach.c
@@ -2,6 +2,27 @@
 #include"prime.h"
 #define RANGE 101
 int judge;
+int add_judge(int b);
+int main()
+{
+    int flag=0;
+    printf("是否需要看证明步骤？\n请输入1代表要看，输入0代表不看\n");
+    scanf("%d",&judge);
+    for(int i=4;i<RANGE;i+=2)
+    {
+        if(!add_judge(i))
+        {
+            printf("哥德巴赫猜想在100以内不成立\n");
+            flag+=1;
+            break;
+        }
+    }
+    if(!flag)
+    {
+        printf("哥德巴赫猜想在100以内成立\n");
+    }
+    return 0;
+}
 int add_judge(int b)
 {
     for(int k=2,l;k<RANGE;k++)
@@ -23,24 +44,3 @@ int add_judge(int b)
         }
     return 0;//如果不存在，就返回0
 }
-int main()
-{
-    int flag=0;
-    printf("是否需要看证明步骤？\n请输入1代表要看，输入0代表不看\n");
-    scanf("%d",&judge);
-    for(int i=4;i<RANGE;i+=2)
-    {
-        if(!add_judge(i))
-        {
-            printf("哥德巴赫猜想在100以内不成立\n");
-            flag+=1;
-            break;
-        }
-    }
-    if(!flag)
-    {
-        printf("哥德巴赫猜想在100以内成立\n");
-    }
-    return 0;
-}
-
