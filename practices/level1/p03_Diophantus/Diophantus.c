a73dffd64645f3495e48c55d3e9a44bf9fab695a
DCjanus <DCjanus@163.com>
2016-03-23 21:56:34 +0800

    一次性交租

--- practices/level1/p03_Diophantus/Diophantus.c
+++ practices/level1/p03_Diophantus/Diophantus.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #define GCD 12//Greatest Common Divisor
+#define MAXAGE 1000//age that I can't imagine
 int main()
 {
-	int age=12;
-	for(;age<200;)
-	{
-
-	}
+    for(float age=GCD;age<MAXAGE;age+=GCD)
+    {
+        if((age/12+age/7+age/6+5+age/2+4)==age)
+        {
+            printf("丢番图丧子时%.0f岁\n",age-4);
+            break;
+        }
+    }
+    return 0;
 }
--- practices/level1/p04_ narcissus/narcissus.c
+++ practices/level1/p04_ narcissus/narcissus.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#define _POW(x,y,z) x*x*x+y*y*y+z*z*z
 int main()
 {
-	return 0;
+    for(int i=1;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            for(int k=0;k<10;k++)
+            {
+                int temp=_POW(i,j,k);
+                if(i*100+j*10+k==temp)
+                {
+                    printf("%d\n",temp);
+                }
+            }
+        }
+    }
+    return 0;
 }
