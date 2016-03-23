f424cbdd647bde11b9468376c999b58aef863f04
HELLHELLO <498523614@qq.com>
2016-03-23 16:45:47 +0800

    linklist gengzheng

--- practices/level1/p11_linkedList/linkedList.c
+++ practices/level1/p11_linkedList/linkedList.c
@@ -12,6 +12,43 @@ typedef struct list{
     point *last;
 } linkList;
 
+void addList(linkList *p);
+linkList* setLinkList(void);
+int serchfirst(int n,linkList* p);
+int serchSecond(int n,linkList* p);
+void resetLinkList(linkList *s);
+
+int main()
+{
+    linkList* s=NULL;
+    s=setLinkList();
+    int x;
+    printf("请输入数据，输入end结束\n");
+    while(scanf("%d",&x)==1){
+        addList(s);
+        s->last->a=x;
+    }
+    resetLinkList(s);
+    printf("%d\n",serchFirst(5,s));
+    printf("%d",serchSecond(5,s));
+    return 0;
+}
+
+void resetLinkList(linkList *s){
+    point *p,*temp1=NULL,*temp2=NULL;
+    p=s->head;
+    for(int i=1;i<=(s->last->number);i++){
+        temp1=p->next;
+        p->next=temp2;
+        p->number=(s->last->number)+1-i;
+        temp2=p;
+        p=temp1;
+    }
+    temp1=s->head;
+    s->head=s->last;
+    s->last=temp1;
+}
+
 void addList(linkList *p){
     int i;
     if(p->head==NULL){
@@ -48,7 +85,7 @@ int serchfirst(int n,linkList* p){
     return i;
 }
 
-int serchsecond(int n,linkList* p){
+int serchSecond(int n,linkList* p){
     point* x=p->head;
     int i=-1,j=2;
     do{
@@ -62,36 +99,4 @@ int serchsecond(int n,linkList* p){
         x=x->next;
     } while((x->next!=NULL));
     return i;
-}
-
-void resetLinkList(linkList *s){
-    point *p,*temp1=NULL,*temp2=NULL;
-    int i;
-    p=s->head;
-    for(i=1;i<=(s->last->number);i++){
-        temp1=p->next;
-        p->next=temp2;
-        p->number=(s->last->number)+1-i;
-        temp2=p;
-        p=temp1;
-    }
-    temp1=s->head;
-    s->head=s->last;
-    s->last=temp1;
-}
-
-int main()
-{
-    linkList* s=NULL;
-    s=setLinkList();
-    int x;
-    printf("请输入数据，输入end结束\n");
-    while(scanf("%d",&x)==1){
-        addList(s);
-        s->last->a=x;
-    }
-    resetLinkList(s);
-    printf("%d\n",serchfirst(5,s));
-    printf("%d",serchsecond(5,s));
-    return 0;
 }
