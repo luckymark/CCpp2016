6e89de93a1458344db63755cf65ad0e892bf7fe2
chenYe <cyanskywing@outlook.com>
2016-03-23 13:30:52 +0800

    unfinished

---
+++ practices/level1/p09_maze/maze.java
@@ -0,0 +1,3 @@
+public void public static void main(String[] args) {
+        System.out.println("hello,world");
+}
--- practices/level1/p11_linkedList/linkedList.cpp
+++ practices/level1/p11_linkedList/linkedList.cpp
@@ -5,13 +5,24 @@ struct node{
     int x;
     node* next = NULL;
 };
+void printnode(node temp);
+//程序主体
 int main(){
     node first;
     node temp = first;
     for(int i = 0;i<100;i++){
         node *p = (node *)malloc(sizeof(node));//int *p = (int *)malloc(sizeof(int)*10);
-        temp.next = p;
         p -> x = i;
+        temp.next = p;
         temp = *p;
     }
+    printnode(first);
+    return 0;
+}
+
+void printnode(node temp){
+    do{
+        temp = *temp.next;
+        printf("%d\n",temp.x);
+    }while(temp.next!=NULL);
 }
