9df974e017b86d461e9786a9b9ac7285cce72040
Flash-Tang <1253957752@qq.com>
2016-03-28 12:36:20 +0800

    重做isPrime

--- practices/level1/p02_isPrime/main.c
+++ practices/level1/p02_isPrime/main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void primeOrNot(int a)
 {
-    int a,i,sum=0;
-    scanf("%d",&a);
+    int i,sum=0;
     for(i=2;i<=a-1;i++)
     {
         int b=a%i;
         if(b==0)
         {
-            sum+=1;
+            sum++;
         }
     }
     if(sum==0)
@@ -21,5 +20,11 @@ int main()
     {
         printf("not a prime indeed\n");
     }
+}
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    primeOrNot(a);
     return 0;
 }
