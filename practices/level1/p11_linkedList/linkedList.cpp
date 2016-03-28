351e86561386ff5f912467ac4e6b8d444e7f6115
Akemi-Homura <xionglinlin2@gmail.com>
2016-03-28 22:16:55 +0800

    complete linkList!

--- practices/level1/p11_linkedList/linkedList.cpp
+++ practices/level1/p11_linkedList/linkedList.cpp
@@ -16,21 +16,34 @@ int main()
     initData(head);
     ergodicLinkList(head);
     reverseLinkList(head);
-    // int firstPos=findValue(head,wantTofind);
-    // printf("%d\n",firstPos);
-    // int nextPos=findValue(head+firstPos+1,wantTofind)+firstPos;
-    // printf("%d\n",nextPos);
+    printf("After reverstion:\n");
+    ergodicLinkList(head);
+    int firstPos=findValue(head,wantTofind);
+    if(firstPos==-1)
+    {
+        printf("%d is not find\n",wantTofind);
+        return 0;
+    }
+    printf("The first position is %d\n",firstPos);
+    int nextPos=findValue(head+firstPos+1,wantTofind)+firstPos;
+    if(nextPos==-1)
+    {
+        printf("%d is only exist once\n",wantTofind);
+        return 0;
+    }
+    printf("The second positon is %d\n",nextPos);
+    return 0;
 }
 void initData(node *&head)
 {
     int temp;
+    node *now=head;
     while(scanf("%d",&temp)==1)
     {
-        node *now=(node*)malloc(sizeof(node));
+        now->next=(node*)malloc(sizeof(node));
+        now=now->next;
         now->value=temp;
         now->next=NULL;
-        head->next=now;
-        head=now;
     }
 }
 void ergodicLinkList(node *head)
@@ -43,16 +56,18 @@ void ergodicLinkList(node *head)
 void reverseLinkList(node *&head)
 {
     node *now=head->next;
-    node *pre=head;
+    node *pre=NULL;
     node *after=now->next;
     while(after!=NULL)
     {
         node *temp=after->next;
-        now->next=pre;  after->next=now;
-        pre=now; now=after; after=temp;
+        now->next=pre;  
+        after->next=now;
+        pre=now; 
+        now=after; 
+        after=temp;
     }
-    head->next=NULL;
-    head=pre;
+    head->next=now;
 }
 int findValue(node *head,int exceptValue)
 {
