bb7e0bb48155ca8069c9b97506caa9c4d9618c78
turiver <kxz11@qq.com>
2016-03-30 22:48:43 +0800

    修改

--- practices/level1/p02_isPrime/prime.cpp
+++ practices/level1/p02_isPrime/prime.cpp
@@ -2,12 +2,7 @@
 #include<cstdio>
 #include<cmath>
 using namespace std;
-bool isPrime(int x)
-{
-	for(int i=2;i<=sqrt(x);i++)
-		if(x%i==0) return 0;
-	return 1;
-}
+bool isPrime(int x);
 int main()
 {
 	int a;
@@ -15,4 +10,10 @@ int main()
 	if(isPrime(a)) printf("Yes");
 		else printf("no");
 	return 0;
+}
+bool isPrime(int x)
+{
+	for(int i=2;i<=sqrt(x);i++)
+		if(x%i==0) return 0;
+	return 1;
 }
