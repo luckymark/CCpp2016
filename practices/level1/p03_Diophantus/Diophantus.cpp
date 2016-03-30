fdff6640dd28ccfe641f87482ad0cd18b9a90e63
fkh12345 <1739321550@qq.com>
2016-03-30 22:45:21 +0800

    Update Diophantus.cpp

--- practices/level1/p03_Diophantus/Diophantus.cpp
+++ practices/level1/p03_Diophantus/Diophantus.cpp
@@ -2,12 +2,12 @@
 #include<stdlib.h>
 int main()
 {
-	for (double i = 0; i < 200.0; i++)
+	for (float i = 0; i < 200.0; i++)
 	{
 		if (i / 6.0 + i / 12.0 + i / 7.0 + i / 2.0 + 5 + 4 == i)
-			{
-				printf("%.0lf\n", i);
-			}
+		{
+			printf("%.0f\n", i);
+		}
 	}
 	system("pause");
 }
