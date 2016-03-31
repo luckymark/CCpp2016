cd9ffd1c1fb06563d39d9889b40edbda7d40274f
iam24 <625083280@qq.com>
2016-03-31 08:53:17 +0800

    add queue

---
+++ practices/cpp/level1/p01_Queue/Queue/Queue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Queue
+//
+//  Created by iam24 on 16/3/31.
+//  Copyright © 2016年 iam24. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
