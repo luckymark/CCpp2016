3077b7c88af8c1d004e6ea7c585e666a221c390c
EriCZZZZ <ericzhao96@hotmail.com>
2016-04-02 12:35:03 +0800

    修改 1.缩进 2.location -> index 3. 注释

--- practices/cpp/level1/p02_Stack/Eric/IntStack.h
+++ practices/cpp/level1/p02_Stack/Eric/IntStack.h
@@ -2,25 +2,19 @@
 #define INT_STACK_H
 class IntStack
 {
-    public:
+public:
     IntStack() = default;
-    //IntStack(int stackCapacity) : num(new int[stackCapacity]), fullHead(stackCapacity) {};
     IntStack(int stackCapacity)
     {
         num = new int[stackCapacity];
         fullHead = stackCapacity;
     } 
-    //echo the stackCapacity
     int showStackCapacity();
-    //入栈
     bool push(int num);
-    //出栈
     int pop();
-    //is stack full
     bool isFull();
-    //is stack empty
     bool isEmpty();
-    private:
+private:
     int *num;
     int head = -1;
     int fullHead = 100;
--- practices/cpp/level1/p03_SafeArray/Eric/main.cpp
+++ practices/cpp/level1/p03_SafeArray/Eric/main.cpp
@@ -9,7 +9,7 @@ int main()
     SafeArray safeArray(100);
     safeArray.set(1, 0);
     safeArray.set(2, 100);
-    std::cout << "The loc.1 " << safeArray.get(1) << std::endl;
-    std::cout << "The loc.100(wrong)" << safeArray.get(100) << std::endl;
+    std::cout << "The index.1 " << safeArray.get(1) << std::endl;
+    std::cout << "The index.100(wrong)" << safeArray.get(100) << std::endl;
     return 0;
 }
