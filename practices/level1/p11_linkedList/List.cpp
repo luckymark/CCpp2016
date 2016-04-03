dcf6b428f7bb23391eeca528b56981c25add07c8
Fragile-azalea <1872891695@qq.com>
2016-04-03 09:28:55 +0800

    Update List.cpp

--- practices/level1/p11_linkedList/List.cpp
+++ practices/level1/p11_linkedList/List.cpp
@@ -1,100 +1,79 @@
-#include<cstdio>
-#include<cstdlib>
-#include<stack>
-struct List {
-    int value = 0, num = 0;
-    List *next = NULL;
-
-    void add(int value) {
-        List *now = this;
-        while (now->next != NULL) {
-            now = now->next;
-        }
-        List *myNext = (List *) malloc(sizeof(List));
-        myNext->next = NULL;
-        myNext->num = now->num + 1;
-        myNext->value = value;
-        now->next = myNext;
+#include "List.h"
+#include <stack>
+void List::add(int value) {
+    List *now = this;
+    while (now->next != NULL) {
+        now = now->next;
     }
-
-    void detele() {
-        if (this->next == NULL) {
-            printf("Error!\n");
-            return;
-        }
-        List *now = this;
-        while (now->next != NULL) {
-            now = now->next;
-        }
-        free(now);
-        List *pre = this;
-        while (pre->next != now) {
-            pre = pre->next;
-        }
-        pre->next = NULL;
-        now = NULL;
+    List *myNext = new List(value, now->num + 1);
+    now->next = myNext;
+}
+void List::remove() {
+    if (this->next == NULL) {
+        printf("Error!\n");
+        return;
     }
-
-    void view() {
-        List *now = this->next;
-        while (now != NULL) {
-            printf("%d ", now->value);
-            now = now->next;
-        }
-        printf("\n");
+    List *now = this;
+    while (now->next != NULL) {
+        now = now->next;
     }
-
-    int find(int number) {
-        List *now = this->next;
-        while (now != NULL) {
-            if (now->value == number) {
-                return now->num;
-            }
-            now = now->next;
-        }
-        return -1;
+    delete now;
+    List *newFinal = this;
+    while (newFinal->next != now) {
+        newFinal = newFinal->next;
     }
-
-    int nextFind(int prePosition, int number) {
-        List *now = this->next;
-        while (now != NULL && now->num <= prePosition) {
-            now = now->next;
-        }
-        while (now != NULL) {
-            if (now->value == number) {
-                return now->num;
-            }
-            now = now->next;
-        }
-        return -1;
+    newFinal->next = NULL;
+}
+void List::view() {
+    List *now = this->next;
+    while (now != NULL) {
+        printf("%d ", now->value);
+        now = now->next;
     }
-
-    List *reverse() {
-        List *now = this->next;
-        std::stack<int> stack1;
-        while (now != NULL) {
-            stack1.push(now->value);
-            now = now->next;
+    puts("");
+}
+int List::find(int number) {
+    List *now = this->next;
+    while (now != NULL) {
+        if (now->value == number) {
+            return now->num;
         }
-        List *newList = (List *) malloc(sizeof(List));
-        newList->next = NULL;
-        newList->value = 0;
-        newList->num = 0;
-        while (!stack1.empty()) {
-            detele();
-            newList->add(stack1.top());
-            stack1.pop();
-        }
-        free(this);
-        return newList;
+        now = now->next;
     }
-};
-int main() {
-    List *myList = (List *) malloc(sizeof(List));
-    myList->value = 0;
-    myList->num = 0;
-    myList->next = NULL;
-    free(myList);
-    myList = NULL;
-    return 0;
+    return -1;
+}
+int List::nextFind(int prePosition, int number) {
+    List *now = this->next;
+    while (now != NULL && now->num <= prePosition) {
+        now = now->next;
+    }
+    while (now != NULL) {
+        if (now->value == number) {
+            return now->num;
+        }
+        now = now->next;
+    }
+    return -1;
+}
+void List::kill() {
+    List *now = this;
+    while (now != NULL) {
+        delete now;
+        now = now->next;
+    }
+}
+List *List::reverse() {
+    List *now = this->next;
+    std::stack<int> stack;
+    while (now != NULL) {
+        stack.push(now->value);
+        now = now->next;
+    }
+    List *newList = new List;
+    this->kill();
+    while (!stack.empty()) {
+        newList->add(stack.top());
+        stack.pop();
+    }
+    return newList;
 }
