56219d7c3749cada9e63cc54d2f2e799be18b549
zhonghouyu <993837013@qq.com>
2016-04-03 10:58:30 +0800

    homework level1

---
+++ practices/level1/hanoi.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void hanoi(int n,char from,char by,char to);
+void move(char from,char to);
+int main(){
+	int n=2;
+	hanoi(n,'A','B','C');
+	return 0;
+}
+
+void hanoi(int n,char from,char by,char to){
+	if(1==n){
+		move(from,to);
+		return;
+	}
+	hanoi(n-1,from,to,by);
+	move(from,to);
+	hanoi(n-1,by,from,to);
+
+}
+void move(char from,char to){
+	printf("%c->%c \n",from,to);
+}
