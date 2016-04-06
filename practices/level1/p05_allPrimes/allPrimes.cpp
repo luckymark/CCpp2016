f900c0d90b411b6a876e5a46ac85d230b0044f9b
uestc-zgl <931524842@qq.com>
2016-04-06 22:52:51 +0800

    Update and rename soeasy to allPrimes.cpp

---
+++ practices/level1/p05_allPrimes/allPrimes.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#define Maxn 1100
+#define Up 500
+using namespace std;
+
+int i,use[Maxn],j;
+int main()
+{
+    for (i=2;i<=Up;i++)
+    if (use[i]==0)
+    {
+        for (j=i+i;j<=Up*2;j+=i) use[j]=1;
+    }
+    for (i=2;i<=Up*2;i++)
+    if (use[i]==0)
+    {
+        printf("%d\n",i);
+    }
+}
