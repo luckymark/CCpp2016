a7dda0a2384a0845691e626571cada5b69058554
zhy <993837013@qq.com>
2016-04-07 18:41:48 +0800

    Update allprimes.cpp

--- practices/level1/allprimes.cpp
+++ practices/level1/allprimes.cpp
@@ -5,10 +5,10 @@ int main(){
 	int i,j,k;
 	clock_t start, end;
 	start = clock();
-	for(i=2;i<=1000;i++){
+	    for(i=2;i<=1000;i++){
 		int sign=0;
 		for(j=2;j<i;j++){
-			k=i%j;
+		    k=i%j;
 			if(k==0){
 				sign=sign+1;
 			}
@@ -17,7 +17,7 @@ int main(){
 			}
 		}
 		if(sign==0){
-			printf("%d \n",i);
+		    printf("%d \n",i);
 		}
 	}
 	end = clock();
