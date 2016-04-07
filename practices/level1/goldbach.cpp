25b4deb8981fb78af70779937bf5a497ca889148
zhy <993837013@qq.com>
2016-04-07 18:46:07 +0800

    Update goldbach.cpp

--- practices/level1/goldbach.cpp
+++ practices/level1/goldbach.cpp
@@ -1,33 +1,33 @@
 #include<stdio.h>
 int main(){
-	int num,i,j,a[100],sign=0;
-	for(num=1;num<=100;num++){
-		for(i=2;i<num;i++){
-			if((num%i!=0)){
-				sign++;
-			}
+    int num,i,j,a[100],sign=0;
+    for(num=1;num<=100;num++){
+    	for(i=2;i<num;i++){
+    	    if((num%i!=0)){
+		sign++;
 		}
-		if(sign==0){
-			a[i-1]=num;
-		}
-		else{
-			continue;
+	}
+	    if(sign==0){
+		a[i-1]=num;
+	    }
+	     else{
+		 continue;
 		}
 	}
 	for(num=1;num<=100;num++){
-		for(i=1;i<=100;i++){
-			for(j=1;j<=100;j++){
-				if(a[i]+a[j]==num){
-					sign++;
-					}
-				}	
-			}
+	    for(i=1;i<=100;i++){
+		for(j=1;j<=100;j++){
+		    if(a[i]+a[j]==num){
+		        sign++;
+				}
+			}	
 		}
+	}
 	if(sign==0){
-		printf("no");
-		}
+            printf("no");
+	}
 	else
-		printf("yes");	
+	    printf("yes");	
 }
 
 	
