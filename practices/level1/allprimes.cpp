9f172b654cddc501c5c69938aa320b8337b08e2f
zhy <993837013@qq.com>
2016-04-07 19:06:07 +0800

    Update allprimes.cpp

--- practices/level1/allprimes.cpp
+++ practices/level1/allprimes.cpp
@@ -2,24 +2,24 @@
 #include <time.h>
 
 int main(){
-	int i,j,k;
-	clock_t start, end;
-	start = clock();
-	    for(i=2;i<=1000;i++){
-		int sign=0;
-		for(j=2;j<i;j++){
-		    k=i%j;
-			if(k==0){
-				sign=sign+1;
-			}
-			else{
-				break;
-			}
+    int i,j,k;
+    clock_t start, end;
+    start = clock();
+    for(i=2;i<=1000;i++){
+	int sign=0;
+	for(j=2;j<i;j++){
+            k=i%j;
+		if(k==0){
+		     sign=sign+1;
 		}
-		if(sign==0){
-		    printf("%d \n",i);
+		else{
+		     break;
 		}
 	}
-	end = clock();
-	printf("The time was: %f s\n", (double)(end - start) / CLK_TCK);
+	if(sign==0){
+	    printf("%d \n",i);
+	}
+    }
+    end = clock();
+    printf("The time was: %f s\n", (double)(end - start) / CLK_TCK);
 }
