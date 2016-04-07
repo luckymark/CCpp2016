f767d2f4f7207905a6de6af511fb17e541f64a70
xian <pzhxbz@QQ.com>
2016-04-07 08:54:21 +0800

    Update prime.h

--- practices/level1/p02_isPrime/prime.h
+++ practices/level1/p02_isPrime/prime.h
@@ -1,25 +1,4 @@
 #ifndef PRIME_H_INCLUDED
 #define PRIME_H_INCLUDED
-int is_prime(int a)
-{
-    int flag=0;
-    for(int i=2;i<a;i++)
-    {
-        if(a%i==0)
-        {
-           flag+=1;
-           break;
-        }
-    }
-    if(flag==0)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-
+int is_prime(int a);
 #endif // PRIME_H_INCLUDED
