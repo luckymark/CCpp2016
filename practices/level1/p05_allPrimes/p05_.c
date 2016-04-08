25cc99fcc8a8a70af7003c37691895f70b9052bb
phq1234 <978005581@qq.com>
2016-04-08 22:43:10 +0800

    修改了p05

--- practices/level1/p05_allPrimes/p05_.c
+++ practices/level1/p05_allPrimes/p05_.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 #include<math.h>
 #include<time.h>
-int a[1000]={0};
+#define NUM 1000
+int a[NUM]={0};
 int main()
 {
     clock_t start_clock=clock();
     long i,j;
-    for(i=2;i<=trunc(sqrt(1000));i++)
+    for(i=2;i<=trunc(sqrt(NUM));i++)
     {
         if(0==a[i])
         {
-            for(j=2;j<=(1000/i);j++)
+            for(j=2;j<=(NUM/i);j++)
             {
                 a[i*j]=1;
             }
         }
     }
-    for(i=2;i<=1000;i++)
+    for(i=2;i<=NUM;i++)
     {
         if(0==a[i])
         {
