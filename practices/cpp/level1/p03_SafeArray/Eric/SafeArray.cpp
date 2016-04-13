a90597e95fcc1cf690d9deae169890a9646b7461
Eric <ericzhao96@hotmail.com>
2016-04-13 23:37:22 +0800

    SafeArrayTip2

--- practices/cpp/level1/p03_SafeArray/Eric/SafeArray.cpp
+++ practices/cpp/level1/p03_SafeArray/Eric/SafeArray.cpp
@@ -1,6 +1,19 @@
 #include "SafeArray.h"
 
-// SafeArray::SafeArray(const SafeArray &source)
+SafeArray::SafeArray(const SafeArray &source)
+{
+	auto temp = new int[source.capacity];
+	for(int count = 0; count != source.capacity; count++)
+	{
+		*(temp + count) = *(source.array + count);
+	}
+	if(array != nullptr)
+	{
+		delete array;
+	}
+	array = temp;
+	capacity = source.capacity;
+}
 bool SafeArray::isSafe(int index) const
 {
 	return (index < capacity) && (index >= LEAST_INDEX);
--- practices/cpp/level1/p03_SafeArray/Eric/SafeArray.h
+++ practices/cpp/level1/p03_SafeArray/Eric/SafeArray.h
@@ -13,21 +13,7 @@ private:
 	bool isSafe(int index) const;
 public:
 	SafeArray(int cap) : capacity(cap), array(new int[cap]){}
-	SafeArray(const SafeArray &source)
-	{
-		auto temp = new int[source.capacity];
-		for(int count = 0; count != source.capacity; count++)
-		{
-			*(temp + count) = *(source.array + count);
-		}
-		if(array != nullptr)
-		{
-			delete array;
-		}
-		array = temp;
-		capacity = source.capacity;
-	}
-
+	SafeArray(const SafeArray &source);
 	int &operator[](int index)
 	{
 		if(isSafe(index))
