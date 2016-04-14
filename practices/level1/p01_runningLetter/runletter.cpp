fb517dd74973a401ed6e631fe4e9bbfdb63b74c0
luoao <bolo4963@gmail.com>
2016-04-14 18:09:48 +0800

    改好了

--- practices/level1/p01_runningLetter/runletter.cpp
+++ practices/level1/p01_runningLetter/runletter.cpp
@@ -1,36 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdio.h>
 #include<windows.h>
 #include<stdlib.h>
+
 int main()
 {
-	int h;
-	for (int i = 0; i < 10000; i++) {
-		for (int j = 0; j <= 78; j++) {
-			for (int k = 0; k < i; k++)
-			{
-				printf(" ");
-			}
-			printf("L");
-			for (int k = j; k <= 78; k++)
-			{
-				printf(" ");
-			}
-			Sleep(30);
-			system("cls");
+	int j = 0;
+	bool h = 0;
+	while(1)
+	{
+		for (int k = 0; k < j; k++)
+		{
+			printf(" ");
 		}
-		for (int j = 78; j >= 1; j--) {
-			for (int k = 0; k < j; k++)
-			{
-				printf(" ");
-			}
-			printf("L");
-			for (int k = j; k <= 78; k++)
-			{
-				printf(" ");
-			}
-			Sleep(30);
-			system("cls");
-		}
+		printf("L");
+		Sleep(1);
+		system("cls");
+		if (j == 78)
+			h = 1;
+		if (j == 0)
+			h = 0;
+		if (h == 0)
+			j++;
+		if (h == 1)
+			j--;
 	}
 	return 0;
 }
