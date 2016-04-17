911657e07cb87fe6ba20050b835a9df86cf8117d
Skuld0 <598734241@qq.com>
2016-04-17 21:48:56 +0800

    Update Diophantus.c

--- practices/level1/p03_Diophantus/Diophantus.c
+++ practices/level1/p03_Diophantus/Diophantus.c
@@ -4,11 +4,11 @@ int main()
 {
     for(int i=2;i<200;i++)
     {
-        if(i%12==0&&i%7==0)
+        if(i%12==0 && i%7==0)
         {
             if((i-9-i/6-i/12-i/7)==(i/2))
             {
-            printf("%d\n",i-4);
+                printf("%d\n",i-4);
             break;
             }
         }
