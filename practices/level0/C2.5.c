5242e5fe3d472b9f29b9f76c9f4dd63901f848cf
Skuld0 <598734241@qq.com>
2016-04-17 22:01:03 +0800

    Update C2.5.c

--- practices/level0/C2.5.c
+++ practices/level0/C2.5.c
@@ -4,9 +4,13 @@ int main()
     int a[100],n,i;
     scanf("%d",&n);
     for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
+    {
+        scanf("%d",&a[i]);
+    }
     for(i=n-1;i>=0;i--)
-    printf("%d ",a[i]);
+    {
+        printf("%d ",a[i]);
+    }
     printf("\n");
     return 0;
 }
