d12ef8a927f2904e8cf02a94b17bf139d322ffda
zhy <993837013@qq.com>
2016-04-18 13:00:07 +0800

    Update diophantus.cpp

--- practices/level1/diophantus.cpp
+++ practices/level1/diophantus.cpp
@@ -3,7 +3,7 @@ int main(){
     int age,i;
     for(age=12;age<=200;age=age+12){
 	  if((age/2)==age-age/6-age/12-age/7-9){
-              i=age-4;
+             i=age-4;
 	      printf("%d",i);
     	  }
     }
