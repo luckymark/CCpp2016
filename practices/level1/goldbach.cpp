587b4468ad681bfdeb063b2ac53cf9040b7da0a6
zhy <993837013@qq.com>
2016-04-18 12:15:08 +0800

    Update goldbach.cpp

--- practices/level1/goldbach.cpp
+++ practices/level1/goldbach.cpp
@@ -5,30 +5,30 @@ int main(){
     	for(i=2;i<num;i++){
     	    if((num%i!=0)){
 		sign++;
-		}
+	    }
 	}
 	    if(sign==0){
 		a[i-1]=num;
 	    }
-	     else{
-		 continue;
+	    else{
+		continue;
+	    }
+    }
+    for(num=1;num<=100;num++){
+	for(i=1;i<=100;i++){
+	    for(j=1;j<=100;j++){
+		if(a[i]+a[j]==num){
+		    sign++;
 		}
+	    }	
 	}
-	for(num=1;num<=100;num++){
-	    for(i=1;i<=100;i++){
-		for(j=1;j<=100;j++){
-		    if(a[i]+a[j]==num){
-		        sign++;
-		    }
-			}	
-		}
-	}
-	if(sign==0){
-            printf("no");
-	}
-	else{
-	    printf("yes");
-	}
+    }
+    if(sign==0){
+         printf("no");
+    }
+    else{
+	printf("yes");
+    }
 }
 
 	
