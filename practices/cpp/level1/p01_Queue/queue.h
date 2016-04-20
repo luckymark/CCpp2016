2038b54ff484a6753c982faf04c03dd39a7d0090
zhy <993837013@qq.com>
2016-04-20 21:46:55 +0800

    Update queue.h

--- practices/cpp/level1/p01_Queue/queue.h
+++ practices/cpp/level1/p01_Queue/queue.h
@@ -1,12 +1,12 @@
 class Queue
 {
 public:
-	void attend(int item);
-	int pop();
-	bool isEmpty();
-	bool isFull();
+    void attend(int item);
+    int pop();
+    bool isEmpty();
+    bool isFull();
 private:
-	int data[100];
-	int head = 0;
-	int tail = 0;
+    int data[100];
+    int head = 0;
+    int tail = 0;
 };
