b492be1d7b1d0a1e2d87c4f6e99c7e1fc7e20fdf
zhy <993837013@qq.com>
2016-04-20 21:45:59 +0800

    Update queue.cpp

--- practices/cpp/level1/p01_Queue/queue.cpp
+++ practices/cpp/level1/p01_Queue/queue.cpp
@@ -1,15 +1,15 @@
 #include"queue.h"
 void Queue::attend(int item) {
-	data[head] = item;
-	head = (head + 1) % 100;
+    data[head] = item;
+    head = (head + 1) % 100;
 }
 int Queue::pop() {
-	tail = (tail + 1) % 100;
-	return data[tail-1];
+    tail = (tail + 1) % 100;
+    return data[tail-1];
 }
 bool Queue::isEmpty() {
-	return(head == tail);
+    return(head == tail);
 }
 bool Queue::isFull() {
-	return ((head + 1) % 100 == tail);
+    return ((head + 1) % 100 == tail);
 }
