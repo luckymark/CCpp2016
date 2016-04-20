12c82890da5b77145f7633154722b1fa53ffd4ed
zhy <993837013@qq.com>
2016-04-20 21:48:37 +0800

    Update main.cpp

--- practices/cpp/level1/p02_Stack/main.cpp
+++ practices/cpp/level1/p02_Stack/main.cpp
@@ -2,17 +2,17 @@
 #include"stack.h"
 using namespace std;
 int main() {
-	int num;
-	cout << "input the size of the stack" << endl;
-	cin >> num;
-	Stack s(num);
-	while (!s.isFull(num)) {
-		s.push(100);
-	}
-	while (!s.isEmpty()) {
-		int n;
-		n = s.pop();
-		cout << n << endl;
-	}
-	system("pause");
+    int num;
+    cout << "input the size of the stack" << endl;
+    cin >> num;
+    Stack s(num);
+    while (!s.isFull(num)) {
+    	s.push(100);
+    }
+    while (!s.isEmpty()) {
+    	int n;
+    	n = s.pop();
+	cout << n << endl
+    }
+    system("pause");
 }
