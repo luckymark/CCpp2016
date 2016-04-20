f8225dc85669ca35bce7f5fa0bed9efdf92aff32
zhy <993837013@qq.com>
2016-04-20 21:52:29 +0800

    Update stack.h

--- practices/cpp/level1/p02_Stack/stack.h
+++ practices/cpp/level1/p02_Stack/stack.h
@@ -1,22 +1,19 @@
 class Stack {
 public:
-	Stack(int num) {
-		data = new int[num];
-		tail = num;
-	}
-	~Stack() {
-		delete[]data;
-	}
+    Stack(int num) {
+	data = new int[num];
+	tail = num;
+    }
+    ~Stack() {
+    	delete[]data;
+    }
 	void push(int item);
 	int pop();
 	bool isFull(int num);
 	bool isEmpty();
 private:
-	int head = 0;
-	int tail;
-	int *data;
-};
-class S :public Stack {
-
+    int head = 0;
+    int tail;
+    int *data;
 };
 
