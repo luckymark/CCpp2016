c476842a6ca03e584dd9bbc42d878d15e44dd022
Skuld0 <598734241@qq.com>
2016-04-20 23:43:43 +0800

    Update C1.1.c

--- practices/level0/C1.1.c
+++ practices/level0/C1.1.c
@@ -6,9 +6,9 @@ int main()
     scanf("%d",&x);
     if(x<0)
         printf("该数为负数");
-        else printf("该数为正数");
+    else printf("该数为正数");
     if(x%2==0)
         printf("且为偶数\n");
     else printf("且为奇数\n");
-        return 0;
+return 0;
 }
