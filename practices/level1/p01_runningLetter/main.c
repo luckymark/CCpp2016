0a6ff42bf46c72bb5d76f02422166f0bbf9e2a36
Flash-Tang <1253957752@qq.com>
2016-04-20 22:29:18 +0800

    2ed_runningLetter

--- practices/level1/p01_runningLetter/main.c
+++ practices/level1/p01_runningLetter/main.c
@@ -5,18 +5,9 @@
 int main()
 {
     int i,j;
-    for(i=0;i<=CONSOLE_WIDTH-1;i++)
+    for(i=0;i<=2*CONSOLE_WIDTH+1;i++)
     {
-        for(j=0;j<=i;j++)
-        {
-            printf(" ");
-        }
-        printf("A");
-        system("cls");
-    }
-    for(i=CONSOLE_WIDTH-2;i>=0;i--)
-    {
-        for(j=0;j<=i;j++)
+        for(j=0;j<=CONSOLE_WIDTH-abs(CONSOLE_WIDTH-i);j++)
         {
             printf(" ");
         }
