c950876d7593a0bf3c8b67155478ea798f2c6730
zhy <993837013@qq.com>
2016-04-20 21:44:52 +0800

    Update main.cpp

--- practices/cpp/level1/p01_Queue/main.cpp
+++ practices/cpp/level1/p01_Queue/main.cpp
@@ -2,14 +2,14 @@
 #include"queue.h"
 using namespace std;
 int main() {
-	Queue queue;
-	while(!queue.isFull()) {
-		queue.attend(100);
-	}
-	int n;
-	while (!queue.isEmpty()) {
-		n = queue.pop();
-		cout << n << endl;
-	}
-	system("pause");
+    Queue queue;
+    while(!queue.isFull()) {
+	queue.attend(100);
+    }
+    int n;
+    while (!queue.isEmpty()) {
+	n = queue.pop();
+	cout << n << endl;
+    }
+    system("pause");
 }
