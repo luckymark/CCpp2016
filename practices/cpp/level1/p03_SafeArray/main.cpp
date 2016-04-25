ea87c11a71fd657ef232763b76dfc6d830689674
zhy <993837013@qq.com>
2016-04-25 19:28:44 +0800

    Update main.cpp

--- practices/cpp/level1/p03_SafeArray/main.cpp
+++ practices/cpp/level1/p03_SafeArray/main.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 #include"safearray.h"
 int main() {
-	safearray s(100);
-	while (1) {
-		if (!s.safe(100)) {
-			s.putin(100);
-		}
-		else {
-			cout << "overload"<<endl;
-			break;
-		}
+    safearray s(100);
+    while (1) {
+	if (!s.safe(100)) {
+	    s.putin(100);
 	}
+	else {
+	    cout << "overload"<<endl;
+	    break;
+	}
+    }
 }
