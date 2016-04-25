a7a9e485eedcfed13f1e9a699dfcc658c78f339b
zhy <993837013@qq.com>
2016-04-25 19:31:27 +0800

    Update safearray.cpp

--- practices/cpp/level1/p03_SafeArray/safearray.cpp
+++ practices/cpp/level1/p03_SafeArray/safearray.cpp
@@ -1,8 +1,8 @@
 #include"safearray.h"
 void safearray::putin(int item) {
-	data[i] = item;
-	i++;
+    data[i] = item;
+    i++;
 }
 bool safearray::safe(int item) {
-	return i <= item;
+    return i <= item;
 }
