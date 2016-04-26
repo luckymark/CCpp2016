2e498da5182a3a009b668153d143f1414f9f362c
Chx <826919861@qq.com>
2016-04-26 21:17:58 +0800

    removed the xcodeproj

--- practices/level1/p10_pushBoxes/pushBoxes/pushBoxes/main.c
+++ practices/level1/p10_pushBoxes/pushBoxes/pushBoxes/main.c
@@ -7,61 +7,104 @@
 //
 
 #include <stdio.h>
-
+#include <string.h>
 #define WIDTH 40
 #define HEIGHT 12
+
+char path1[120]="/Users/caoxiann/Desktop/c/c\ \&c++lesson/CCpp2016/practices/level1/p10_pushBoxes/pushBoxes/pushBoxes/map1.txt";
+char path2[120]="/Users/caoxiann/Desktop/c/c\ \&c++lesson/CCpp2016/practices/level1/p10_pushBoxes/pushBoxes/pushBoxes/map2.txt";
 
 void printMap();
 void judgeDirection(char step);
 void ifHitSth(int tempX,int tempY);
 void run(int nextX,int nextY);
 void pushBox(int tempX,int tempY);
-
-int finished,x,y;
+void getMap(int index);
+int finished,x,y,boxes=0,quit=0;
 char map[HEIGHT][WIDTH]={
-    "************************************",
-    "************************************",
-    "**    *************** x     ********",
-    "**    *****************     ********",
-    "** o                     ***********",
-    "**                       ***********",
-    "******    ***********    ***********",
-    "******   #**************************",
-    "******    **************************",
-    "************************************",
-    "************************************",
-    "************************************",
+//    "************************************",
+//    "************************************",
+//    "**    *************** x     ********",
+//    "**    *****************     ********",
+//    "** o                     ***********",
+//    "**                       ***********",
+//    "******    ***********    ***********",
+//    "******   #**************************",
+//    "******    **************************",
+//    "************************************",
+//    "************************************",
+//    "************************************",
     
     
 };
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    
-    
-    finished=0;
-    printMap();
-    printf("请将箱子# 推到指定位置 x\n");
-    printf("请输入wasd + enter移动\n");
-    char step;
-    y=4;
-    x=3;
-    while (finished==0) {
+    int index=0;
+    while (quit==0) {
+        index++;
+        finished=0;
+        boxes=0;
+        getMap(index);
+        printMap();
+        printf("请将箱子# 推到指定位置 x\n");
+        printf("请输入wasd + enter移动\n");
+        char step;
+        while (finished!=boxes) {
+            
+            scanf("%c",&step);
+            judgeDirection(step);
+            
+        }
         
-        scanf("%c",&step);
-        judgeDirection(step);
-        
+        printf("恭喜你推倒了箱子!\n");
+        printf("请问还要玩下一关吗?  y/n  \n");
+        char isQuit;
+        scanf("%c",&isQuit);
+        scanf("%c",&isQuit);
+        if (isQuit=='y') {
+            quit=0;
+        }else{
+            quit=1;
+        }
     }
+    printf("欢迎再次玩耍\n");
     
-    printf("恭喜你推倒了箱子!\n");
     return 0;
 }
-
+void getMap(int index){
+    char path[120];
+    if (index==1) {
+        strcpy(path, path1);
+    }else if (index==2){
+        strcpy(path, path2);
+    }
+    FILE *fr=fopen(path, "r");
+    if (fr==NULL) {
+        printf("地图打开失败\n");
+    }else{
+        int i=0,j;
+        while (fgets(map[i], 80, fr)) {
+            for (j=0; j<WIDTH; j++) {
+                if (map[i][j]=='o') {
+                    y=i;
+                    x=j;
+                }
+                if (map[i][j]=='#') {
+                    boxes++;
+                }
+            }
+            i++;
+        }
+    }
+    
+    fclose(fr);
+}
 void printMap(){
     for (int i=0; i<HEIGHT; i++) {
         for (int j=0; j<WIDTH; j++) {
-            if (j==WIDTH-1) {
-                printf("\n");
-            }
+           
+            
             printf("%c",map[i][j]);
         }
     }
@@ -95,13 +138,22 @@ void ifHitSth(int tempX,int tempY){
 
         pushBox(tempX, tempY);
        
+    }else if (nextStep=='@'){
+        pushBox(tempX, tempY);
     }
 }
 
 void run(int nextX,int nextY){
     char temp=map[y][x];
-    map[y][x]=map[nextY][nextX];
-    map[nextY][nextX]=temp;
+    if (temp!='^') {
+        map[y][x]=map[nextY][nextX];
+        map[nextY][nextX]=temp;
+    }else{
+       
+        map[y][x]='x';
+        map[nextY][nextX]='o';
+        
+    }
     x=nextX;
     y=nextY;
     system("clear");
@@ -110,20 +162,38 @@ void run(int nextX,int nextY){
 
 void pushBox(int tempX,int tempY){
     
+    
     int nextX,nextY;
     char nextStep=map[tempY][tempX];
     nextX=2*tempX-x;
     nextY=2*tempY-y;
     char nextOfBox=map[nextY][nextX];
-
+    
+    
     if (nextOfBox==' ') {
-        map[nextY][nextX]=nextStep;
-        map[tempY][tempX]=map[y][x];
-        map[y][x]=nextOfBox;
+        
+        if (map[tempY][tempX]=='@') {
+            
+            map[nextY][nextX]='#';
+            map[tempY][tempX]='^';
+            map[y][x]=' ';
+            finished--;
+            
+        }else if (map[y][x]=='^'){
+            
+            map[y][x]='x';
+            map[tempY][tempX]='o';
+            map[nextY][nextX]='#';
+            
+        }else {
+            map[nextY][nextX]=nextStep;
+            map[tempY][tempX]=map[y][x];
+            map[y][x]=nextOfBox;
+            
+            
+        }
         system("clear");
         printMap();
-        
-        
         y=tempY;
         x=tempX;
         
@@ -135,6 +205,8 @@ void pushBox(int tempX,int tempY){
         map[y][x]=' ';
         system("clear");
         printMap();
-        finished=1;
+        y=tempY;
+        x=tempX;
+        finished++;
     }
 }
