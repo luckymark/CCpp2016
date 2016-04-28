46be2e04a00e8b9bdacd42bf1bc6418485f7f1e8
chenYe <cyanskywing@outlook.com>
2016-04-29 00:09:46 +0800

    exist many bugs

--- practices/level1/p10_pushBoxes/pushBox.cpp
+++ practices/level1/p10_pushBoxes/pushBox.cpp
@@ -10,17 +10,19 @@ const int width = 10;//尺寸暂定为10x10
 const int height = 10;
 int boxMap[height][width];
 struct object{
+    int index;
     int positionX;
     int positionY;
 } player,box,goal;
 //-----------------------------
 bool readFile(int level);
-void move(object ob,char direction);
+bool move(object &ob,char direction);
 void play();
 void showMap();
 char shape(int i);
-void bind(object ob,int x,int y);
-bool can_move(object ob,char direction);
+void bind(object &ob,int index,int x,int y);
+bool can_move(object &ob,char direction);
+void changeTo(object ob,int index);
 //-----------------------------
 
 int main() {
@@ -56,9 +58,9 @@ bool readFile(int level) {
             }
             boxMap[i][j] = temp-'0';
             switch(boxMap[i][j]) {
-                case 2:bind(player,j,i);boxMap[i][j] = 0;break;
-                case 3:bind(goal,j,i);break;
-                case 5:bind(box,j,i);boxMap[i][j] = 0;break;
+                case 2:bind(player,boxMap[i][j],j,i);boxMap[i][j] = 0;break;
+                case 3:bind(goal,boxMap[i][j],j,i);break;
+                case 5:bind(box,boxMap[i][j],j,i);boxMap[i][j] = 0;break;
             }
             j++;
         }
@@ -70,13 +72,7 @@ void showMap() {
     system("cls");
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            if((j == box.positionX)&&(i == box.positionY)) {
-                cout << shape(5);
-            }else if((j == player.positionX)&&(i == player.positionY)) {
-                cout << shape(2);
-            }else{
-                cout << shape(boxMap[i][j]);
-            }
+            cout << shape(boxMap[i][j]);
             if(j == (width-1)) {
                 cout << endl;
             }
@@ -101,33 +97,59 @@ void play() {
     while(true) {
         char direction;
         showMap();
-        direction = getch();
+         direction = getch();
         move(player,direction);
     }
 }
-void move(object ob,char direction) {
+//控制移动
+bool move(object &ob,char direction) {
     if(can_move(ob,direction)){
+        changeTo(ob,0);
         switch (direction) {
             case 'w':ob.positionY --;break;
             case 's':ob.positionY ++;break;
             case 'a':ob.positionX --;break;
             case 'd':ob.positionX ++;break;
         }
+        changeTo(ob,ob.index);
+        return true;
+    }else{
+        return false;
     }
 }
 //绑定位置
-void bind(object ob,int x,int y) {
+void bind(object &ob,int index,int x,int y) {
+    ob.index = index;
     ob.positionX = x;
     ob.positionY = y;
 }
 //是否有阻碍
-bool can_move(object ob,char direction) {
+bool can_move(object &ob,char direction) {
     bool flag = 1;
     switch (direction) {
-        case 'w':if(boxMap[ob.positionY-1][ob.positionX] == 1)flag = 0;break;
-        case 's':if(boxMap[ob.positionY+1][ob.positionX] == 1)flag = 0;break;
-        case 'a':if(boxMap[ob.positionY][ob.positionX-1] == 1)flag = 0;break;
-        case 'd':if(boxMap[ob.positionY][ob.positionX+1] == 1)flag = 0;break;
+        case 'w':{
+            if(boxMap[ob.positionY-1][ob.positionX] == 5)flag = move(box,'w');
+            if(boxMap[ob.positionY-1][ob.positionX] == 1)flag = 0;
+            break;
+        }
+        case 's':{
+            if(boxMap[ob.positionY+1][ob.positionX] == 5)flag = move(box,'s');
+            if(boxMap[ob.positionY+1][ob.positionX] == 1)flag = 0;
+            break;
+        }
+        case 'a':{
+            if(boxMap[ob.positionY][ob.positionX-1] == 5)flag = move(box,'a');
+            if(boxMap[ob.positionY][ob.positionX-1] == 1)flag = 0;
+            break;
+        }
+        case 'd':{
+            if(boxMap[ob.positionY][ob.positionX+1] == 5)flag = move(box,'d');
+            if(boxMap[ob.positionY][ob.positionX+1] == 1)flag = 0;
+            break;
+        }
     }
     return flag;
+}
+void changeTo(object ob,int index) {
+    boxMap[ob.positionY][ob.positionY] = index;
 }
