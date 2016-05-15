de97a7088d9dd094f10b31215f599166d3a7933f
turiver <kxz11@qq.com>
2016-05-15 19:41:02 +0800

    改

--- practices/cpp/level1/p01_Queue/Queue.h
+++ practices/cpp/level1/p01_Queue/Queue.h
@@ -3,11 +3,10 @@
 class Queue
 {
  public:
-	void append(int item);
-    int pop();
-	bool is_empty();
-    bool is_full();
-
+ 	void append(int item);
+ 	int pop();
+ 	bool is_empty();
+ 	bool is_full();
  private:
     int data[100];
     int tail=0;
