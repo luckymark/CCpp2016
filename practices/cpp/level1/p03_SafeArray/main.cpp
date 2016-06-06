35c5a8b35d5c97d4d38a5fc5ab28180418ca6b9d
iam24 <625083280@qq.com>
2016-06-06 19:38:48 +0800

    template

--- practices/cpp/level1/p03_SafeArray/main.cpp
+++ practices/cpp/level1/p03_SafeArray/main.cpp
@@ -1,23 +1,41 @@
-#include<iostream>
-#include "queque.h"
+//
+//  main.cpp
+//  stack
+//
+//  Created by iam24 on 16/3/31.
+//  Copyright © 2016年 iam24. All rights reserved.
+//
 
-int main()
-{
-    int a = 5;
-    int *pa;
-    pa = &a;
-    std::cout<<pa;
-    Queue queue;
-    int n  = 10;
-    if (!queue.is_full())
+#include <iostream>
+#include "safe_array.hpp"
+int main() {
+    int capbility;
+    std::cout<<"输入数组大小："<<std::endl;
+    std::cin>>capbility;
+    Safe_array<double> safe_array(capbility);
+    std::cout<<"初始化数组元素："<<std::endl;
+    for (int i = 0 ; i < capbility ;i++)
     {
-        queue.append(n);
+        std::cout<<"第"<<i<<"个元素："<<std::endl;
+        double value;
+        std::cin>>value;
+        safe_array[i] = value;
     }
-    if (!queue.is_empty())
-    {
-        
-        n = queue.pop();
-        std::cout<<n;
+    //    for (int i = 0 ;i < capbility ;i++)
+    //        safe_array.set(i, i*10-2); //初始化
+    std::cout<<"请输入需要修改元素的位置和值"<<std::endl;
+    int n;
+    double value;
+    std::cin>>n>>value;
+    try{
+        safe_array[n] = value;
+    } catch (const char * msg){
+        std::cout<<msg<<std::endl;
     }
+    for (int i = 0 ;i < capbility ; i++)
+        std::cout<<safe_array[i]<<" ";
+    //    safe_array.set(n, value);//赋值
+    //    std::cin>>n;
+    //    safe_array.get(n);//读取值
     return 0;
 }
--- practices/cpp/level1/p03_SafeArray/safe_array.hpp
+++ practices/cpp/level1/p03_SafeArray/safe_array.hpp
@@ -9,21 +9,42 @@
 #ifndef safe_array_hpp
 #define safe_array_hpp
 
-#include <stdio.h>
+template <class T>
+
 class Safe_array
 {
 public:
-    Safe_array(int capbility);
+    Safe_array(int capbility){
+        data = new T[capbility];
+        all = capbility;
+    };
     ~Safe_array()
     {
         delete data;
     }
-    void set(int n ,int value);
-    void get(int n);
-    void check_index(int n );
-    int &operator [] (int a);
+    void set(int n, T value)
+    {
+        check_index(n);
+        data[n] = value;
+        std::cout<<"赋值成功!"<<std::endl;
+    }
+    void get(int n)
+    {
+        check_index(n);
+        std::cout<<"值为"<<data[n]<<std::endl;
+    }
+    void check_index(int n)
+    {
+        if (n>=all || n<0)
+            throw "数组访问越界！";
+    }
+    T &operator[](int a)
+    {
+        check_index(a);
+        return data[a];
+    }
 private:
     int all;
-    int *data;
+    T *data;
 };
 #endif /* safe_array_hpp */
