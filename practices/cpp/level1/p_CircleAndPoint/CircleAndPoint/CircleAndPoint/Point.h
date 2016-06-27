d75a326ed186a54f8dde560e8a4e241480314a31
Akemi-Homura <xionglinlin2@gmail.com>
2016-06-27 20:34:05 +0800

    完成了Circle & Point

---
+++ practices/cpp/level1/p_CircleAndPoint/CircleAndPoint/CircleAndPoint/Point.h
@@ -0,0 +1,22 @@
+#pragma once
+#pragma once
+#include <iostream>
+class Point
+{
+public:
+
+	Point(int x, int y) :x(x), y(y) {}
+	~Point() {}
+	void draw();
+	bool operator<(const Point&rhs)const
+	{
+		return (x < rhs.x) || (x == rhs.x && y < rhs.y);
+	}
+	Point operator+(const Point&rhs)const
+	{
+		return Point(x + rhs.x, y + rhs.y);
+	}
+private:
+	int x, y;
+};
+
